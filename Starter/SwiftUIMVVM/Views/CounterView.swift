810b087097d5a7e223dd4ff7d6420f6957a6a868
kazuooooo <matsumotokazuya7@gmail.com>
2020-06-30 17:23:04 +0900

    :tada: Add Starter

---
+++ Starter/SwiftUIMVVM/Views/CounterView.swift
@@ -0,0 +1,21 @@
+//
+//  CounterView.swift
+//  SwiftUIMVVM
+//
+//  Created by 松本和也 on 2020/06/30.
+//  Copyright © 2020 松本和也. All rights reserved.
+//
+
+import SwiftUI
+
+struct CounterView: View {    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountView_Previews: PreviewProvider {
+    static var previews: some View {
+        CounterView()
+    }
+}
